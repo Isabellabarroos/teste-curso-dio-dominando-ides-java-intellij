b82bb63eed974a152feb2197c872c6cd1a297f4c
Isabella <isabella.ffb@hotmail.com>
2022-07-14 17:23:57 -0300

    Criando class livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -4,15 +4,52 @@ import br.com.dio.model.Gato;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
-    Gato gato = new Gato();
-
+        Gato gato = new Gato();
         System.out.println(gato);
+
+        Livro livro= new Livro("Anne de Green Galbes",  336);
+        System.out.println(livro);
 
         /*int a =5 ;
         int b = 3;
         System.out.println("Hello World!! " + (a+b));*/
     }
 }
+
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
+}
+
 
 /*comandos
 ctrl+shift+/= comenta um bloco inteiro
