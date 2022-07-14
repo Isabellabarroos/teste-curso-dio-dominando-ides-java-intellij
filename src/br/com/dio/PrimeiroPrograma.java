0e6cc9bb1242bf0c6937f2ee99cdd5652479d0fc
Isabella <isabella.ffb@hotmail.com>
2022-07-14 16:56:05 -0300

    First commit Intellij

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,26 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+    Gato gato = new Gato();
+
+        System.out.println(gato);
+
+        /*int a =5 ;
+        int b = 3;
+        System.out.println("Hello World!! " + (a+b));*/
+    }
+}
+
+/*comandos
+ctrl+shift+/= comenta um bloco inteiro
+ctrl+/= comenta linha por linha
+ctrl+y= deleta a linha
+ctrl+z= cria a linha
+shift+f6= renomeia a classe
+ctrl+alt+l= organiza as linhas
+ctrl+d= duplica a linha
+ctrl+shift+seta para cima ou para baixo= muda a linha de posição
+ctrl+k= commit */
